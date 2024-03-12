f98f27cc9171b4a2c274702ab2ee9d430d6bc171
Alex-programist-python-and-bat <119322118+Alex-programist-python-and-bat@users.noreply.github.com>
2024-03-12 16:07:34 +0500

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print("hello") 
-time.sleep(10)
-print("world")
+def calculate_pi(terms):
+    pi = 0
+    for k in range(terms):
+        pi += ((-1) ** k) / (2 * k + 1)
+    return 4 * pi
 
+num_terms = 1000000
+print(calculate_pi(num_terms))
