82f6948894f55de838e32720783e51997d536a57
Alex-programist-python-and-bat <119322118+Alex-programist-python-and-bat@users.noreply.github.com>
2024-03-12 16:10:59 +0500

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import time
+from tqdm import tqdm
+
 def calculate_pi(terms):
     pi = 0
-    for k in range(terms):
+    for k in tqdm(range(terms), desc="Calculating pi", unit="term"):
         pi += ((-1) ** k) / (2 * k + 1)
+        time.sleep(0.0001)  # для задержки иллюстрации
     return 4 * pi
 
-num_terms = 1000000
+num_terms = 10000000000000000000000000
 print(calculate_pi(num_terms))
